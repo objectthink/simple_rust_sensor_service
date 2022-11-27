4bf4bb314692a5e4fb2e9735a865de53a20650ed
objectthink <objectthink@main.hsd1.pa.comcast.net>
2022-11-27 11:17:17 -0500

    store sensor location changes in memory, implement location request

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,43 @@
 #[macro_use]
 extern crate log;
 
-use std::collections::HashMap;
 use futures::stream::StreamExt;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Sensor {
     id: String,
-    location: String
+    location: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), async_nats::Error> {
     env_logger::init();
 
-    let mut sensors = HashMap::<String, Sensor>::new();
-
     let mut handles = vec![];
-
     let client = async_nats::connect("nats://localhost:4222").await?;
+
+    // let mut sensors = HashMap::<String, Sensor>::new();
+    // let mut shared_sensors = Arc::new(sensors);
+
+    // how to use hashmap across threads/tasks with Arc and Mutex
+
+    // initialize the map within an Arc (for sharing) and a Mutex (for synchronization)
+    //https://stackoverflow.com/questions/39045636/how-do-i-have-one-thread-that-deletes-from-a-hashmap-and-another-that-inserts
+    let sensors: Arc<Mutex<HashMap<String, Sensor>>> = Arc::new(Mutex::new(HashMap::new())); 
+
+    // clone the Arc so it can be owned jointly by multiple threads
+    let sensors_events = sensors.clone(); 
+    let sensors_get = sensors.clone();
 
     // spawn a task - listen for PROPERTY_CHANGED events
     handles.push(tokio::task::spawn({
         let client = client.clone();
+
         async move {
             let mut subscriber = client.subscribe("*.event".to_string()).await?;
 
@@ -42,20 +56,24 @@ async fn main() -> Result<(), async_nats::Error> {
                     let response = client
                         .request(format!("{}.get", sensor), "location".into())
                         .await?;
-                    
+
                     info!("get response: {:#?}", response);
 
                     let location = String::from_utf8_lossy(&response.payload.to_vec()).to_string();
 
-                    sensors.insert(sensor.to_string(), Sensor{
-                        id: sensor.to_string(),
-                        location: String::from(&location)
-                    });
-
+                    // get the sensors hashmap from mutex
+                    sensors_events.lock().unwrap().insert(
+                        sensor.to_string(),
+                        Sensor {
+                            id: sensor.to_string(),
+                            location: String::from(&location),
+                        },
+                    );
+                    
                     // store the location in memory and on disk for when the sensor asks for it below
                     std::fs::write(
                         "/tmp/test.json",
-                        serde_json::to_string_pretty(&sensors).unwrap(),
+                        serde_json::to_string_pretty(&*sensors_events.lock().unwrap()).unwrap()
                     )?;
                 }
             }
@@ -67,23 +85,21 @@ async fn main() -> Result<(), async_nats::Error> {
     // spawn a task that processes get location requests
     handles.push(tokio::task::spawn({
         let client = client.clone();
+
         async move {
             let mut subscriber = client.subscribe("000.get.location".to_string()).await?;
 
             println!("Awaiting get requests");
             while let Some(message) = subscriber.next().await {
-                if message.payload == "380" {
-                    info!("got message from: {}", message.subject);
+                // use the message payload to get the location
+                let sensor_id = String::from_utf8_lossy(&message.payload.to_vec()).to_string();
 
-                    // get the sensor id from the subject
-                    let mut tokens_iter = message.subject.split(".");
-                    let sensor = tokens_iter.next().unwrap();
+                info!("requesting location for {}", sensor_id);
 
-                    info!("sensor {} property changed", sensor);
+                // get the requested sensor location from sensors
+                let sensor = sensors_get.lock().unwrap().get(&sensor_id).unwrap().clone();
 
-                    // get the new location from the sensor
-                    // store the location in memory and on disk for when the sensor asks for it below
-                }
+                info!("location for sensor {:#?} is {:#?}", sensor, sensor.location);
             }
 
             Ok::<(), async_nats::Error>(())
